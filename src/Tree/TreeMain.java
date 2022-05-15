a6dffbb33d4995c2e694857c0b5a1e5d9b3b8016
iamvirgoo <carens.kha4atryan@yandex.ru>
2022-05-15 23:23:16 +0300

    tree_v3

--- src/Tree/TreeMain.java
+++ src/Tree/TreeMain.java
@@ -2,18 +2,57 @@ package Tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
-/**
- *
- * @author w w w. j a v a g i s t s . c o m
- *
- */
 class TreeMain {
     public static void main(String[] args) {
-        Node<String> root = createTree();
-        printTree(root, " ");
+        Scanner scan = new Scanner(System.in);
+        Node<String> root = new Node<>("root");
+
+        /*Node<String> root = createTree();
+        printTree(root, "-");*/
+
+        boolean flag = false;
+        String gap = "    ";
+        System.out.println("""
+                1 - Generate tree
+                3 - Output all tree
+                0 - Exit
+                """);
+        /*Node<String> parent = null;
+        Node<String> leaf = null;*/
+        while (!flag) {
+            System.out.print("Enter the number of operation: ");
+            int OperationNumber = scan.nextInt();
+            switch (OperationNumber) {
+                case 1 -> {
+                    System.out.print(gap + "Enter the tree length: ");
+                    int length = scan.nextInt();
+                    for (int i = 0; i < length; i++) {
+                        Node<String> parent = root.addChild(new Node<>(getString(5)));
+                        for (int j = 0; j < length; j++) {
+                            Node<String> leaf = parent.addChild(new Node<>(getString(5)));
+                        }
+                    }
+                }
+                case 2 -> {
+                    System.out.print("Enter the parent value: ");
+                    String par = scan.next();
+                    Node<String> newParent = new Node<>(par);
+                    root.setParent(newParent);
+                }
+                case 3 -> {
+                    System.out.println();
+                    printTree(root, "-");
+                    System.out.println();
+                }
+                case 0 -> flag = true;
+            }
+        }
+
     }
-    private static Node<String> createTree() {
+    /*private static Node<String> createTree() {
         Node<String> root = new Node<>("root");
         Node<String> node1 = root.addChild(new Node<String>("node 1"));
         Node<String> node11 = node1.addChild(new Node<String>("node 11"));
@@ -24,7 +63,19 @@ class TreeMain {
         Node<String> node21 = node2.addChild(new Node<String>("node 21"));
         Node<String> node211 = node2.addChild(new Node<String>("node 22"));
         return root;
+    }*/
+
+    public static String getString(int length) {
+        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Random random = new Random();
+        StringBuilder Sbuffer = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(52);
+            Sbuffer.append(str.charAt(number));
+        }
+        return Sbuffer.toString();
     }
+
     private static <T> void printTree(Node<T> node, String appender) {
         System.out.println(appender + node.getData());
         node.getChildren().forEach(each ->  printTree(each, appender + appender));
@@ -33,7 +84,7 @@ class TreeMain {
 
         private T data = null;
 
-        private List<Node<T>> children = new ArrayList<>();
+        private final List<Node<T>> children = new ArrayList<>();
 
         private Node<T> parent = null;
 
